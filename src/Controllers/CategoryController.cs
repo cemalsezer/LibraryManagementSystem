d464601938c5b1be964c999d48daad61e2b99ed9
CemalSezer <infocemalsezer@gmail.com>
2024-10-18 16:24:12 +0300

    db connect operations are builded

---
+++ src/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystem.Models.Entity;
+namespace LibraryManagementSystem.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var values = db.CATEGORY.ToList();
+            return View(values);
+        }
+    }
+}
