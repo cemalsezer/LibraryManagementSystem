28b6065f97bc6fbba755e83cd77aac61a1ba1506
CemalSezer <infocemalsezer@gmail.com>
2024-10-21 11:43:03 +0300

    book view file is updated

---
+++ src/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystem.Models.Entity;
+namespace LibraryManagementSystem.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var books = db.BOOK.ToList();
+            return View(books);
+        }
+    }
+}
