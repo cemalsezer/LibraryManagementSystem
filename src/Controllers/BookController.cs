0c90dda0e8467d4b6ae1106295584aebbf2781d2
CemalSezer <infocemalsezer@gmail.com>
2024-10-23 22:12:54 +0300

    Book operations are updated

--- src/Controllers/BookController.cs
+++ src/Controllers/BookController.cs
@@ -53,5 +53,39 @@ namespace LibraryManagementSystem.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult BookGet(int id)
+        {
+            var book = db.BOOK.Find(id);
+            List<SelectListItem> value1 = (from i in db.CATEGORY.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.NAME,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.vl1 = value1;
+            List<SelectListItem> value2 = (from i in db.AUTHOR.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.NAME + ' ' + i.SURNAME,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.vl2 = value2;
+            return View("BookGet", book);
+        }
+        public ActionResult BookUpdate(BOOK b)
+        {
+            var book = db.BOOK.Find(b.ID);
+            book.NAME = b.NAME;
+            book.PUBLISHDATE = b.PUBLISHDATE;
+            book.PAGE=b.PAGE;
+            book.PUBLISHER = b.PUBLISHER;
+            var category = db.CATEGORY.Where(c => c.ID == b.CATEGORY1.ID).FirstOrDefault();
+            var author = db.AUTHOR.Where(a=>a.ID == b.AUTHOR1.ID).FirstOrDefault();
+            book.CATEGORY = category.ID;
+            b.AUTHOR = author.ID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
