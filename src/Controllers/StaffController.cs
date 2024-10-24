630ccdde905a6e4225668a9b3a9b50992b27093f
CemalSezer <infocemalsezer@gmail.com>
2024-10-24 12:07:15 +0300

    Personnel asset addition transactions have been created

---
+++ src/Controllers/StaffController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystem.Models.Entity;
+namespace LibraryManagementSystem.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Personel
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var values = db.STAFF.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult StaffAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult StaffAdd(STAFF s)
+        {
+            db.STAFF.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
