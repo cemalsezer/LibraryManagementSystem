a561ce700dbbaa8c27db7c0352de1995e21fed05
CemalSezer <infocemalsezer@gmail.com>
2024-10-30 20:00:58 +0300

    user operations are updated

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LibraryManagementSystem.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
 namespace LibraryManagementSystem.Controllers
 {
     public class UserController : Controller
@@ -11,9 +13,10 @@ namespace LibraryManagementSystem.Controllers
         // GET: User
         DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
 
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            var values = db.USER.ToList();
+            //var values = db.USER.ToList();
+            var values = db.USER.ToList().ToPagedList(page, 5);
             return View(values);
         }
         [HttpGet]
@@ -49,6 +52,13 @@ namespace LibraryManagementSystem.Controllers
         {
             var userUpdate = db.USER.Find(s.ID);
             userUpdate.NAME = s.NAME;
+            userUpdate.SURNAME = s.SURNAME;
+            userUpdate.EMAIL = s.EMAIL;
+            userUpdate.USERNAME = s.USERNAME;
+            userUpdate.PASSWORD = s.PASSWORD;
+            userUpdate.SCHOOLNAME = s.SCHOOLNAME;
+            userUpdate.PHONE = s.PHONE;
+            userUpdate.IMAGE = s.IMAGE;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
