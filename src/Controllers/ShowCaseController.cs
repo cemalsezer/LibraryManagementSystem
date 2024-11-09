398fc8ef90e08154a0a2d9268f629201853386dc
CemalSezer <infocemalsezer@gmail.com>
2024-11-09 14:26:49 +0300

    showcase controller is updated

---
+++ src/Controllers/ShowCaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystem.Models.Entity;
+using LibraryManagementSystem.Models.Classes;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class ShowCaseController : Controller
+    {
+        // GET: ShowCase
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Value1 = db.BOOK.ToList();
+            cs.Value2 = db.ABOUTUS.ToList();
+
+            //var values = db.BOOK.ToList();
+            return View(cs);
+        }
+    }
+}
