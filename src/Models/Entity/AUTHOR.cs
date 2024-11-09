ff35a5308ba435b71dbfffab7eca546884a25ac7
CemalSezer <infocemalsezer@gmail.com>
2024-11-09 16:48:07 +0300

    new dashboard builded and entites are updated

--- src/Models/Entity/AUTHOR.cs
+++ src/Models/Entity/AUTHOR.cs
@@ -11,7 +11,8 @@ namespace LibraryManagementSystem.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class AUTHOR
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,7 @@ namespace LibraryManagementSystem.Models.Entity
         }
     
         public int ID { get; set; }
+        [Required(ErrorMessage ="Name is required!")]
         public string NAME { get; set; }
         public string SURNAME { get; set; }
         public string DETAIL { get; set; }
