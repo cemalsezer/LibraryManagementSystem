864ba950b0b888ad1b715e52e1d37c256c983cb9
CemalSezer <infocemalsezer@gmail.com>
2024-11-23 22:00:18 +0300

    controllers layer is updated

---
+++ src/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using LibraryManagementSystem.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(USER u)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register");
+            }
+            db.USER.Add(u);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
