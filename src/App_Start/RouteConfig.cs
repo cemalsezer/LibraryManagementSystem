34fb0aa09ed065f04a5fe8a77be73dceada97fe1
CemalSezer <infocemalsezer@gmail.com>
2024-11-25 20:50:46 +0300

    app_start is updated

--- src/App_Start/RouteConfig.cs
+++ src/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LibraryManagementSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ShowCase", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
